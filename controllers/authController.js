34fc8da680355eebe3b5051a5389ff2d1dfe1d0f
Your Name <moseoduor86@gmail.com>
2022-05-17 12:06:18 +0300

    changes to authcontroller to include login

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,3 +16,18 @@ export const register = async (req, res, next) => {
     next(err);
   }
 };
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User not found"));
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect) return next(createError(400, "wrong password"));
+
+    res.status(201).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
